76e33320a13892d434112a3df4fcbdb20c04bd60
Azizova-Bella <azizovabella14@gmail.com>
2025-03-19 22:42:26 +0500

    fixxxed

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,12 @@
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
-import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@styles': path.resolve(__dirname, 'src/app/styles'),
-			'@pages': path.resolve(__dirname, 'src/pages'),
-			'@layout': path.resolve(__dirname, 'src/widgets/Layout'),
-			'@shared': path.resolve(__dirname, 'src/shared/assets'),
-		},
-	},
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
